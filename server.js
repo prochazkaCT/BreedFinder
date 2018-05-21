962fd1edb63039d7db5a4aceab9261740b99f1ef
stprocha <sharon@countertools.org>
2018-05-21 17:33:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, "./app/public")));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
